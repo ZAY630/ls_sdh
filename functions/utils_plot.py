032e863d0707a965324811d74635a403a07fb493
Aoyu Zou <zy22021@outlook.com>
2024-04-11 16:06:36 -0700

    Further updated plots y label and removed units from y ticks, adjusted heatmap colorbar

--- functions/utils_plot.py
+++ functions/utils_plot.py
@@ -4,7 +4,21 @@ import numpy as np
 import seaborn as sns
 import matplotlib.dates as mdates
 
-def make_plot(df, axe, columns = [], date = '', unit = '', yrange = [], plot_title = '', figsize = (), color = [], hide_xticks = False, xlabel = '', dual_columns = [], dual_unit = '', conversion_factor = 1, legend=[]):
+def make_plot(df, 
+              axe, 
+              columns = [], 
+              date = '', 
+              yrange = [], 
+              plot_title = '', 
+              color = [], 
+              hide_xticks = False, 
+              xlabel = '', 
+              ylabel = '',
+              dual_columns = [], 
+              dual_ylabel = '', 
+              conversion_factor = 1, 
+              legend=[]):
+    
     if dual_columns:
         df['datetime'] = pd.to_datetime(df.iloc[:, 0])
 
@@ -14,19 +28,18 @@ def make_plot(df, axe, columns = [], date = '', unit = '', yrange = [], plot_tit
         if date != '':
             df = df[df['datetime'].dt.date == pd.to_datetime(date)]
 
-        # Plotting each column against the 'datetime'
-        for column in columns:
-            axe.plot(df['datetime'], df[column], label = column)
+        if color:
+            for idx, column in enumerate(columns):
+                axe.plot(df['datetime'], df[column], lw = 4, color=color[idx], label = legend[idx])
+        else:
+            for idx, column in enumerate(columns):
+                axe.plot(df['datetime'], df[column], lw = 4, label = legend[idx])
 
         plt.title(plot_title, fontsize = 24)
         axe.set_ylim(yrange[0], yrange[1])
-        new_ticks = np.arange(yrange[0], yrange[1] + 1, yrange[2])  # Generate 4 evenly spaced ticks within the current range
+        new_ticks = np.arange(yrange[0], yrange[1], yrange[2])  # Generate 4 evenly spaced ticks within the current range
         axe.set_yticks(new_ticks)
-
-        # Change tick labels to show only the integer part
-        tick_labels = [f"{int(tick)}" for tick in new_ticks[:-1]] + [f"{int(new_ticks[-1])}{unit}"]  # Convert to integer and add "units" to the last label
-        axe.set_yticklabels(tick_labels)
-        axe.set_ylabel(None)
+        axe.set_ylabel(ylabel, fontsize = 22)
         axe.set_title(plot_title, fontsize = 24)
         if hide_xticks:
             axe.set_xticklabels([])
@@ -38,10 +51,11 @@ def make_plot(df, axe, columns = [], date = '', unit = '', yrange = [], plot_tit
             
         # Create the secondary y-axis for kWh/ft2
         ax2 = axe.twinx()
-        new_ticks = np.arange(round(yrange[0] * conversion_factor), round((yrange[1] + 1) * conversion_factor), round(yrange[2] * conversion_factor / 5) * 5)  # Ensure max value is included
         ax2.set_yticks(new_ticks)
-        tick_labels = [f"{round(tick)}" for tick in new_ticks[:-1]] + [f"{round(new_ticks[-1])}{dual_unit}"]
-        ax2.set_yticklabels(tick_labels)
+        secondary_y_tick_labels = [f'{round(y * conversion_factor)}' for y in new_ticks]
+        ax2.set_yticklabels(secondary_y_tick_labels)
+        ax2.set_ylim(yrange[0], yrange[1])
+        ax2.set_ylabel(dual_ylabel, fontsize = 22)
 
         # General plot settings
         axe.tick_params(axis='y', which = 'major', length=10, width=2, labelsize=22)
@@ -50,6 +64,7 @@ def make_plot(df, axe, columns = [], date = '', unit = '', yrange = [], plot_tit
         ax2.tick_params(axis='x', labelsize=22)
 
         axe.set_xlabel(xlabel, fontsize=22)
+        axe.yaxis.grid(True, linestyle='-', color='lightgrey')
 
         # Hide unwanted spines
         axe.spines['top'].set_visible(False)
@@ -62,15 +77,12 @@ def make_plot(df, axe, columns = [], date = '', unit = '', yrange = [], plot_tit
         ax2.spines['bottom'].set_visible(False)
         
     else:
-        df['datetime'] = pd.to_datetime(df.iloc[:, 0])
 
         if columns == []:
             columns = df.columns[1:]
 
         if date != '':
             df = df[df['datetime'].dt.date == pd.to_datetime(date)]
-
-        # plt.figure(figsize=figsize)
 
         # Plotting each column against the 'datetime'
         if color:
@@ -81,32 +93,37 @@ def make_plot(df, axe, columns = [], date = '', unit = '', yrange = [], plot_tit
                 axe.plot(df['datetime'], df[column], lw = 4, label = legend[idx])
 
         axe.set_title(plot_title, fontsize = 24)
-        plt.legend(fontsize=22, loc='upper right')
+        plt.legend(fontsize=18, loc='upper left')
 
         # Add units to y-tick labels
-        new_ticks = np.arange(yrange[0], yrange[1] + 1, yrange[2])  # Adjust the step value as needed
+        new_ticks = np.arange(yrange[0], yrange[1], yrange[2])  # Adjust the step value as needed
 
         # Set the new ticks
         axe.set_yticks(new_ticks)
-
-        # Format tick labels to show only the integer part and add "units" to the last label
-        tick_labels = [f"{int(tick)}" for tick in new_ticks[:-1]] + [f"{int(new_ticks[-1])}{unit}"]
+        axe.set_ylabel(ylabel, fontsize=22)
 
         # Format date ticks
         # Set major locator to 6 hours and use a DateFormatter for the x-axis
         if date:
-            axe.xaxis.set_major_locator(mdates.HourLocator(byhour=[0, 6, 12, 18]))
-            axe.xaxis.set_major_formatter(mdates.DateFormatter('%-I %p'))  # '%I %p' for 12-hour clock format with AM/PM
+            axe.xaxis.set_major_locator(mdates.HourLocator(byhour=[1, 7, 13, 19]))
+            axe.xaxis.set_major_formatter(mdates.DateFormatter('%-I %p', tz=df['datetime'].dt.tz))  # '%I %p' for 12-hour clock format with AM/PM
 
-        axe.set_yticklabels(tick_labels)
         axe.tick_params(axis='both', labelsize=22)
+        axe.tick_params(axis='y', which = 'major', length=10, width=2)
         axe.spines['top'].set_visible(False)
         axe.spines['right'].set_visible(False)
         axe.spines['left'].set_visible(False)
         axe.spines['bottom'].set_visible(False)
+        axe.yaxis.grid(True, linestyle='-', color='lightgrey')
 
 
-def plot_hourly_heatmap(df, columns, annotation='', plot_title = '', cbar_ticks = [], cbar_label = [], figsize = ()):
+def plot_hourly_heatmap(df, 
+                        columns, 
+                        annotation='', 
+                        plot_title = '', 
+                        cbar_ticks = [], 
+                        cbar_label = [], 
+                        figsize = ()):
     
    # Ensure datetime is the correct type for processing
     df['datetime'] = pd.to_datetime(df['datetime'])  # Convert if not already in datetime format
@@ -125,12 +142,13 @@ def plot_hourly_heatmap(df, columns, annotation='', plot_title = '', cbar_ticks 
 
     # Plot heatmap
     plt.figure(figsize=figsize)
-    heatmap = sns.heatmap(heatmap_data, cmap='viridis_r', linewidths=0, cbar_kws={'orientation': 'horizontal', 'pad': 0.075})
+    heatmap = sns.heatmap(heatmap_data, cmap='viridis_r', linewidths=0, cbar_kws={'orientation': 'horizontal', 'pad': 0.1, 'shrink': 0.75})
 
     # Modify color bar label, adding units
     cbar = heatmap.collections[0].colorbar
-    cbar.set_label(annotation, labelpad=-80, fontsize=22)
-    cbar.ax.tick_params(labelsize=18)
+    cbar.set_label(annotation, labelpad=-70, fontsize=22)
+    cbar.ax.tick_params(labelsize=22)
+
     if cbar_ticks:
         cbar.set_ticks(cbar_ticks) 
         cbar.set_ticklabels(cbar_label) 
@@ -149,8 +167,8 @@ def plot_hourly_heatmap(df, columns, annotation='', plot_title = '', cbar_ticks 
     heatmap.set_yticklabels(month_labels, rotation=0)  # Set labels with no rotation
 
     # Set additional labels and titles
-    hours = ['12 AM', '3 AM', '6 AM', '9 AM', '12 PM', '3 PM', '6 PM', '9 PM']
-    hour_positions = [0, 3, 6, 9, 12, 15, 18, 21]  # Corresponding positions in 24-hour format
+    hours = ['12 AM', '6 AM', '12 PM', '6 PM']
+    hour_positions = [0, 6, 12, 18]  # Corresponding positions in 24-hour format
     plt.xticks(hour_positions, labels=hours, rotation = 0)  # Adjust tick positions and labels
     plt.title(plot_title, fontsize=24)
     plt.xticks(fontsize=22)
@@ -160,7 +178,21 @@ def plot_hourly_heatmap(df, columns, annotation='', plot_title = '', cbar_ticks 
     df.reset_index(inplace = True)
 
 
-def create_box_plot(df, columns, unit, axe, plot_title='', annotation = True, hide_xticks = False, xlabel = '', yrange = [], color = [], type = False, dual_columns = [], dual_unit = '', conversion_factor = 1):
+def create_box_plot(df, 
+                    columns, 
+                    unit, 
+                    axe, 
+                    plot_title='', 
+                    annotation = True, 
+                    hide_xticks = False, 
+                    xlabel = '', 
+                    ylabel = '',
+                    yrange = [], 
+                    color = [], 
+                    type = False, 
+                    dual_columns = [], 
+                    dual_ylabel = '', 
+                    conversion_factor = 1):
 
     # Calculate and print mean and median on the plot
     if dual_columns:
@@ -171,7 +203,7 @@ def create_box_plot(df, columns, unit, axe, plot_title='', annotation = True, hi
                     # Adjust the text position (x, y) and alignment as necessary
                     axe.text(row['hour'] - 0.15 if row['type'] == 'high' else row['hour'] + 0.15, 
                             median * 1.01, 
-                            f'{median:.0f}{unit}', 
+                            f'{median:.0f}', 
                             horizontalalignment='center', size='large', color='black', fontsize = 18)
 
             else:
@@ -195,24 +227,22 @@ def create_box_plot(df, columns, unit, axe, plot_title='', annotation = True, hi
 
 
         axe.set_ylim(yrange[0], yrange[1])
-        new_ticks = np.arange(yrange[0], yrange[1] + 1, yrange[2])  # Generate 4 evenly spaced ticks within the current range
+        new_ticks = np.arange(yrange[0], yrange[1], yrange[2])  # Generate 4 evenly spaced ticks within the current range
         axe.set_yticks(new_ticks)
-
-        # Change tick labels to show only the integer part
-        tick_labels = [f"{int(tick)}" for tick in new_ticks[:-1]] + [f"{int(new_ticks[-1])}{unit}"]  # Convert to integer and add "units" to the last label
-        axe.set_yticklabels(tick_labels)
-        axe.set_ylabel(None)
+        axe.set_ylabel(ylabel, fontsize = 22)
         axe.set_title(plot_title, fontsize = 24)
         if hide_xticks:
             axe.set_xticklabels([])
         axe.set_xlabel(xlabel, fontsize = 22)
+        axe.yaxis.grid(True, linestyle='-', color='lightgrey')
 
         # Create the secondary y-axis for kWh/ft2
         ax2 = axe.twinx()
-        new_ticks = np.arange(round(yrange[0] * conversion_factor), round((yrange[1] + 1) * conversion_factor), round(yrange[2] * conversion_factor / 5) * 5)  # Ensure max value is included
         ax2.set_yticks(new_ticks)
-        tick_labels = [f"{round(tick)}" for tick in new_ticks[:-1]] + [f"{round(new_ticks[-1])}{dual_unit}"]
-        ax2.set_yticklabels(tick_labels)
+        secondary_y_tick_labels = [f'{round(y * conversion_factor)}' for y in new_ticks]
+        ax2.set_yticklabels(secondary_y_tick_labels)
+        ax2.set_ylim(yrange[0], yrange[1])
+        ax2.set_ylabel(dual_ylabel, fontsize = 22)
 
         # General plot settings
         axe.tick_params(axis='y', which = 'major', length=10, width=2, labelsize=22)
@@ -242,7 +272,7 @@ def create_box_plot(df, columns, unit, axe, plot_title='', annotation = True, hi
                     # Adjust the text position (x, y) and alignment as necessary
                     axe.text(row['hour'] - 0.15 if row['type'] == 'high' else row['hour'] + 0.15, 
                             median * 1.01, 
-                            f'{median:.0f}{unit}', 
+                            f'{median:.0f}', 
                             horizontalalignment='center', size='large', color='black', fontsize = 18)
 
             else:
@@ -252,7 +282,7 @@ def create_box_plot(df, columns, unit, axe, plot_title='', annotation = True, hi
                     median = df[col].median()   
                     
                     # Annotate the statistics on the plot
-                    axe.text(i, median * 1.025, f'{median:.0f}{unit}', horizontalalignment='center', size='large', color='black', fontsize = 18)
+                    axe.text(i, median * 1.025, f'{median:.0f}', horizontalalignment='center', size='large', color='black', fontsize = 18)
 
 
         # Map median values to colors
@@ -266,13 +296,10 @@ def create_box_plot(df, columns, unit, axe, plot_title='', annotation = True, hi
 
 
         axe.set_ylim(yrange[0], yrange[1])
-        new_ticks = np.arange(yrange[0], yrange[1] + 1, yrange[2])  # Generate 4 evenly spaced ticks within the current range
+        new_ticks = np.arange(yrange[0], yrange[1], yrange[2])  # Generate 4 evenly spaced ticks within the current range
         axe.set_yticks(new_ticks)
 
-        # Change tick labels to show only the integer part
-        tick_labels = [f"{int(tick)}" for tick in new_ticks[:-1]] + [f"{int(new_ticks[-1])}{unit}"]  # Convert to integer and add "units" to the last label
-        axe.set_yticklabels(tick_labels)
-        axe.set_ylabel(None)
+        axe.set_ylabel(ylabel, fontsize = 22)
         axe.set_title(plot_title, fontsize = 24)
         if hide_xticks:
             axe.set_xticklabels([])
@@ -285,7 +312,18 @@ def create_box_plot(df, columns, unit, axe, plot_title='', annotation = True, hi
         axe.spines['left'].set_visible(False)
         axe.spines['bottom'].set_visible(False)
 
-def create_bar_plot(df, columns, unit, plot_title='', annotation = True, xlabel = '', yrange = [], dual_columns = [], dual_unit = '', figsize = (), color = [], conversion_factor = 1):
+def create_bar_plot(df, 
+                    columns, 
+                    plot_title='', 
+                    annotation = True, 
+                    xlabel = '', 
+                    ylabel = '',
+                    yrange = [], 
+                    dual_columns = [], 
+                    dual_ylabel = '', 
+                    figsize = (), 
+                    color = [], 
+                    conversion_factor = 1):
 
     if dual_columns:
 
@@ -296,26 +334,25 @@ def create_bar_plot(df, columns, unit, plot_title='', annotation = True, xlabel 
             axe = df[columns].plot(kind='bar', figsize=figsize)
 
         # Adjust y-tick labels for primary y-axis
-        new_ticks = np.arange(yrange[0], yrange[1] + 1, yrange[2])  # Ensure max value is included
+        new_ticks = np.arange(yrange[0], yrange[1], yrange[2])  # Ensure max value is included
         axe.set_yticks(new_ticks)
-        tick_labels = [f"{int(tick)}" for tick in new_ticks[:-1]] + [f"{int(new_ticks[-1])}{unit}"]
-        axe.set_yticklabels(tick_labels)
         plt.xticks(rotation=0)  # Set x-ticks if df.index is not numeric
+        axe.set_ylim(yrange[0], yrange[1])
 
         # Annotate the primary bars with values
         if annotation:
             for i, anno in enumerate(df[columns]):
                     
                     # Annotate the statistics on the plot
-                    plt.text(i, anno * 1.025, f'{anno:.0f}{unit}', horizontalalignment='center', size='large', color='black', fontsize = 22)
+                    plt.text(i, anno * 1.025, f'{anno:.0f}', horizontalalignment='center', size='large', color='black', fontsize = 22)
 
         # Create the secondary y-axis for kWh/ft2
         ax2 = axe.twinx()
-        ax2.plot(df[columns].index, df[dual_columns], marker='', linestyle='')  # Plot as points
-        new_ticks = np.arange(round(yrange[0] * conversion_factor), round((yrange[1] + 1) * conversion_factor), round(yrange[2] * conversion_factor / 5) * 5)  # Ensure max value is included
         ax2.set_yticks(new_ticks)
-        tick_labels = [f"{round(tick)}" for tick in new_ticks[:-1]] + [f"{round(new_ticks[-1])}{dual_unit}"]
-        ax2.set_yticklabels(tick_labels)
+        secondary_y_tick_labels = [f'{round(y * conversion_factor)}' for y in new_ticks]
+        ax2.set_yticklabels(secondary_y_tick_labels)
+        ax2.set_ylim(yrange[0], yrange[1])
+        ax2.set_ylabel(dual_ylabel, fontsize = 22)
 
         # General plot settings
         axe.tick_params(axis='y', which = 'major', length=10, width=2, labelsize=22)
@@ -325,6 +362,7 @@ def create_bar_plot(df, columns, unit, plot_title='', annotation = True, xlabel 
 
         axe.set_xlabel(xlabel, fontsize=22)
         axe.set_title(plot_title, fontsize=22)
+        axe.set_ylabel(ylabel, fontsize = 22)
 
         # Hide unwanted spines
         axe.spines['top'].set_visible(False)
@@ -335,6 +373,8 @@ def create_bar_plot(df, columns, unit, plot_title='', annotation = True, xlabel 
         ax2.spines['left'].set_visible(False)
         ax2.spines['right'].set_visible(False)
         ax2.spines['bottom'].set_visible(False)
+
+        axe.yaxis.grid(True, linestyle='-', color='lightgrey')
 
         # Show the plot
         plt.show()
@@ -347,15 +387,11 @@ def create_bar_plot(df, columns, unit, plot_title='', annotation = True, xlabel 
         # Add units to y-tick labels
         new_ticks = np.arange(yrange[0], yrange[1], yrange[2])  # Adjust the step value as needed
         axe.set_yticks(new_ticks)
-        
-        if dual_unit:
-            tick_labels = [f"{int(tick)}" for tick in new_ticks[:-1]] + [f"{int(new_ticks[-1])}{unit}\n({round(new_ticks[-1] * conversion_factor)}{dual_unit})"]
-        else:
-            tick_labels = [f"{int(tick)}" for tick in new_ticks[:-1]] + [f"{int(new_ticks[-1])}{unit}"]
-        axe.set_yticklabels(tick_labels)
-        axe.tick_params(axis='both', which = 'major', length=10, width=2, labelsize=22)
+        axe.set_ylabel(ylabel, fontsize = 22)
+        axe.tick_params(axis='y', which = 'major', length=10, width=2, labelsize=22)
+        axe.tick_params(axis='x', which = 'major', labelsize=22)
         plt.xticks(rotation = 0)
-        plt.xlabel(xlabel)
+        plt.xlabel(xlabel, fontsize = 22)
         plt.title(plot_title, fontsize = 24)
         if annotation:
             for i, anno in enumerate(df[columns]):
@@ -367,4 +403,5 @@ def create_bar_plot(df, columns, unit, plot_title='', annotation = True, xlabel 
         axe.spines['right'].set_visible(False)
         axe.spines['left'].set_visible(False)
         axe.spines['bottom'].set_visible(False)
+        axe.yaxis.grid(True, linestyle='-', color='lightgrey')
         plt.show()
